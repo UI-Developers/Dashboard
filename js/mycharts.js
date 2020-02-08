40fe83e8fb98947a57135179049e022bc6e5ac66
UI-Developers <30897145+UI-Developers@users.noreply.github.com>
2020-02-08 21:40:49 +0000

    Update mycharts.js

--- js/mycharts.js
+++ js/mycharts.js
@@ -66,7 +66,7 @@ trafficNav.addEventListener('click', (e) => {
       button.className = 'active';
       myTrafficChart.options.scales.yAxes[0].ticks.max = 800;
       myTrafficChart.options.scales.yAxes[0].ticks.stepSize = 100;
-      myTrafficChart.data.labels = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm"]
+      myTrafficChart.data.labels = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm"];
       myTrafficChart.data.datasets[0].data = [58, 120, 454, 742, 731, 340, 469, 604, 589, 718, 361, 483, 235];
       myTrafficChart.update();
 
@@ -75,7 +75,7 @@ trafficNav.addEventListener('click', (e) => {
       button.className = 'active';
       myTrafficChart.options.scales.yAxes[0].ticks.max = 2500;
       myTrafficChart.options.scales.yAxes[0].ticks.stepSize = 500;
-      myTrafficChart.data.labels = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+      myTrafficChart.data.labels = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
       myTrafficChart.data.datasets[0].data = [205, 1114, 1704, 1690, 1530, 1873, 2086];
       myTrafficChart.update();
 
@@ -85,7 +85,7 @@ trafficNav.addEventListener('click', (e) => {
 
       myTrafficChart.options.scales.yAxes[0].ticks.max = 2500;
       myTrafficChart.options.scales.yAxes[0].ticks.stepSize = 500;
-      myTrafficChart.data.labels = ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"]
+      myTrafficChart.data.labels = ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"];
       myTrafficChart.data.datasets[0].data = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2200];
       myTrafficChart.update();
 
@@ -95,7 +95,7 @@ trafficNav.addEventListener('click', (e) => {
 
       myTrafficChart.options.scales.yAxes[0].ticks.max = 10000;
       myTrafficChart.options.scales.yAxes[0].ticks.stepSize = 1000;
-      myTrafficChart.data.labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+      myTrafficChart.data.labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
       myTrafficChart.data.datasets[0].data = [5200, 4302, 2800, 5553, 5001, 3853, 2063, 1024, 4020, 5601, 7002, 9138];
       myTrafficChart.update();
     }
@@ -108,8 +108,7 @@ trafficNav.addEventListener('click', (e) => {
 -------------------------------*/
 
 let myBarChart = document.getElementById('daily-chart').getContext('2d');
-
-let barChart = new Chart(myBarChart, {
+  let barChart = new Chart(myBarChart, {
   aspectRatio: 2.5,
   animation: {
   duration: 0
@@ -151,7 +150,7 @@ let barChart = new Chart(myBarChart, {
 -------------------------------*/
 
 let myDoughnutChart = document.getElementById('mobile-users').getContext('2d');
-let DoughnutChart = new Chart(myDoughnutChart, {
+  let DoughnutChart = new Chart(myDoughnutChart, {
   aspectRatio: 2.5,
   animation: {
   duration: 0
